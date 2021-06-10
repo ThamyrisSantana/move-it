33bf0b6e855e4d68670b7fa6f46e17f28e3c7cea
Thamyris <thamyrissatana3@gmail.com>
2021-06-10 17:11:24 -0300

    title update

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,12 +2,17 @@ import { CompletedChallenges } from "../components/CompletedChallenges";
 import { Countdown } from "../components/Countdown";
 import { ExperienceBar } from "../components/ExperienceBar";
 import { Profile } from "../components/Profile";
+import Header from "next/head";
 
 import styles from "../styles/pages/Home.module.css";
+import Head from "next/head";
 
 export default function Home() {
   return (
     <div className={styles.container}>
+      <Head>
+        <title>Inicio | move.it</title>
+      </Head>
       <ExperienceBar />
       <section>
         <div>
