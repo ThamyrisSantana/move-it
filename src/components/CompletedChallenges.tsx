ff0e814275d8f25387247ec042a768c8328ae498
Thamyris <thamyrissatana3@gmail.com>
2021-06-10 16:40:03 -0300

    countdown css and completed challenges

--- src/components/CompletedChallenges.tsx
+++ src/components/CompletedChallenges.tsx
@@ -1,3 +1,10 @@
+import styles from "../styles/components/CompletedChallanges.module.css";
+
 export const CompletedChallenges = () => {
-  return <div></div>;
+  return (
+    <div className={styles.completedChallenges}>
+      <span>Desafios completos</span>
+      <span>5</span>
+    </div>
+  );
 };
