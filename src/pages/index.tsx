e9daf5ae9ab49baa2fa842d4e241816d50d4f9d7
Thamyris <thamyrissatana3@gmail.com>
2021-06-10 13:27:48 -0300

    experience-bar added

---
+++ src/pages/index.tsx
@@ -0,0 +1,12 @@
+import { ExperienceBar } from "../components/ExperienceBar";
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <div className="container">
+      <ExperienceBar />
+      <div></div>
+      <div></div>
+    </div>
+  );
+}
